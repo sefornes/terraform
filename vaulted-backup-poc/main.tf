26fed29fe2515cb5b410253a1c52636f3b35aa46
sefornes <41449429+sefornes@users.noreply.github.com>
2023-12-27 09:57:19 +0100

    fix: correct syntax

--- vaulted-backup-poc/main.tf
+++ vaulted-backup-poc/main.tf
@@ -7,16 +7,27 @@ locals {
   location            = "northeurope"
 }
 
+module "log_analytics" {
+  source = "github.com/equinor/terraform-azurerm-log-analytics"
+
+  workspace_name      = "log-testing-sef"
+  resource_group_name = local.resource_group_name
+  location            = local.location
+}
+
 module "storage" {
   source = "github.com/equinor/terraform-azurerm-storage?ref=v12.2.1"
 
   resource_group_name = local.resource_group_name
   location            = local.location
   account_name        = "sttestingsef01"
+
+  log_analytics_workspace_id = module.log_analytics.workspace_id
 }
 
 module "recovery" {
-  source = "github.com/sefornes/terraform/vaulted-backup-poc/module"
+  source     = "github.com/sefornes/terraform/vaulted-backup-poc/module"
+  depends_on = [module.storage]
 
   vault_name          = "rsv-testing-sef"
   resource_group_name = local.resource_group_name
--- vaulted-backup-poc/module/outputs.tf
+++ vaulted-backup-poc/module/outputs.tf
@@ -5,5 +5,5 @@ output "vault_name" {
 
 output "vault_principal_id" {
   description = "The principal id of the system managed identity for the Recovery Services Vault."
-  value       = azurerm_recovery_services_vault.this.identity
+  value       = azurerm_recovery_services_vault.this.identity.principal_id
 }
