dfd73e297dbca78ccc2052e4fedca7f6ce32d102
sefornes <41449429+sefornes@users.noreply.github.com>
2023-12-27 10:02:25 +0100

    fix: correct outputs

--- vaulted-backup-poc/module/outputs.tf
+++ vaulted-backup-poc/module/outputs.tf
@@ -5,5 +5,5 @@ output "vault_name" {
 
 output "vault_principal_id" {
   description = "The principal id of the system managed identity for the Recovery Services Vault."
-  value       = azurerm_recovery_services_vault.this.identity.principal_id
+  value       = azurerm_recovery_services_vault.this.identity[0].principal_id
 }
